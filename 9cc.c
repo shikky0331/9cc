ab41866ffd905fed4b01ba2d793e749e6d58b0a1
shikky0331 <shikky0331>
2021-11-30 09:04:53 +0900

    add a function that can do add and substract.

--- 9cc.c
+++ 9cc.c
@@ -5,14 +5,37 @@ int main(int argc, char **argv)
 {
   if (argc != 2)
   {
-    fprintf(stderr, "引数の個数が正しくありません\n");
+    fprintf(stderr, "number of args aren't correct");
     return 1;
   }
+
+  char *p = argv[1];
 
   printf(".intel_syntax noprefix\n");
   printf(".globl main\n");
   printf("main:\n");
-  printf("  mov rax, %d\n", atoi(argv[1]));
+  printf("  mov rax, 5ld\n", strtol(p, &p, 10));
+
+  while (*p)
+  {
+    if (*p == '+')
+    {
+      p++;
+      printf("  add rax, %ld\n", strtol(p, &p, 10));
+      continue;
+    }
+
+    if (*p == '-')
+    {
+      p++;
+      printf("  sub rax, %ld\n", strtol(p, &p, 10));
+      continue;
+    }
+
+    fprintf(stderr, "not expected string: '%c'\n", *p);
+    return 1;
+  }
+
   printf("  ret\n");
   return 0;
 }
